71ba60330a0a12abaf1f8391c708662121cba11a
Bambang Irawan <99801891+raykairawan@users.noreply.github.com>
2023-09-29 22:35:26 +0700

    Delete challenge-2 directory

--- challenge-2/src/main/java/com/challenge1/exception/IOException.java
+++
@@ -1,7 +0,0 @@
-package com.challenge1.exception;
-
-public class IOException extends RuntimeException {
-    public IOException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- challenge-2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.challenge1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.logging;
-
-
-    opens com.challenge1 to javafx.fxml;
-    exports com.challenge1;
-}
